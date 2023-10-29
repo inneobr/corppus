8842d5274384ea9a08ae0ee1a66b5f4cd7a0b4a6
Eduardo Cruz <65505772+inneobr@users.noreply.github.com>
2023-10-29 15:13:32 -0300

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Demo = () => {
   return (
-    <div className="fixed w-full absolute -right-[20px] -top-[20px] w-[180px] h-[180xp] z-99">
+    <div className="fixed w-full absolute -right-[20px] -top-[20px] w-[185px] h-[185xp] z-99">
       <Image 
         src={'/assets/demo.png'} 
         width={260}
