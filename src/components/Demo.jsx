4dd55559e98b19c2ba0070c744860593bab8b15d
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-10-29 15:21:34 -0300

    feat: update demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const Demo = () => {
   return (
-    <div className="fixed w-full absolute -right-[20px] -top-[20px] w-[180px] h-[180xp] z-99">
+    <div className="fixed w-full absolute -left-[20px] -top-[20px] w-[180px] h-[180xp] z-99">
       <Image 
         src={'/assets/demo.png'} 
         width={180}
         height={180}
-        className='w-[180px] h-[180px]'
+        className='w-[180px] h-[180px] -rotate-90'
         alt=''/>
     </div>
   )
